b2afb3651c19c3832680f320e8dfbad0134ace18
prakmoonhyo <159526751+prakmoonhyo@users.noreply.github.com>
2025-11-09 17:07:21 +0900

    Clean up comments and format output
    Refactor code to remove comments and improve clarity.

--- baek_10871_B5.py
+++ baek_10871_B5.py
@@ -3,17 +3,17 @@
 
 
 # 풀이 1
-# n, x = map(int, input().split())
-# lst = list(map(int, input().split()))
-# min_lst = []
+n, x = map(int, input().split())
+lst = list(map(int, input().split()))
+min_lst = []
 
-# for i in lst:
-#     if i < x:
-#         min_lst.append(i)
+for i in lst:
+    if i < x:
+        min_lst.append(i)
 
-# print(*min_lst) # 자동으로 공백 기준 출력 해줌 
+print(*min_lst) # 자동으로 공백 기준 출력 해줌 
 
-# join을 이용한 풀이 
+# join을 이용한 풀이 2
 
 # words = ["apple", "banana", "cherry"]
 # result = " ".join(words) 이때 join은 문자열만 가능
@@ -21,7 +21,7 @@
 
 # apple banana cherry
 
-#따러서 리스트에 정수 형태를 출력 하려면 
+# 따러서 리스트에 정수 형태를 출력 하려면 
 # " ".join(map(str, lst)) 형태를 사용
 
 n, x = map(int, input().split())
@@ -32,4 +32,6 @@ for i in lst:
     if i < x:
         min_lst.append(i)
 
+
 print(" ".join(map(str, min_lst)))
+# " "을 기준으로 출력해준다.
