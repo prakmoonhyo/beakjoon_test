d553c2a8d337bfe09edd9bcff302445d6d4edae8
prakmoonhyo <159526751+prakmoonhyo@users.noreply.github.com>
2025-11-14 19:57:52 +0900

    Update beak_10811_B2.py

--- beak_10811_B2.py
+++ beak_10811_B2.py
@@ -27,5 +27,5 @@ print(" ".join(map(str, box)))
 # list[1:4:1]의 의미는 [1][2][3]이렇게 가는거고
 # list[4:1:-1]의 의미는 [3][2][1]이렇게 가는거다 
 # 즉 start > end 이면 step < 0이 가능이고 
-
 # start < end 이면 step > 0이 가능이다
+
